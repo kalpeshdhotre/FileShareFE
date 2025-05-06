9e3d5b5c16177722081ff6375e30887403ce49e0
Kalpesh KD <kalpeshdhotre@hotmail.com>
2025-05-06 15:03:08 +0530

    axios config updated for BE

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-const BASE_URL="/api/files/"
-const axiosInstance=axios.create()
+const BASE_URL = "https://filesharebe.onrender.com/api/files/";
+// const BASE_URL="/api/files/"
+const axiosInstance = axios.create();
 
-axiosInstance.defaults.baseURL=BASE_URL;
+axiosInstance.defaults.baseURL = BASE_URL;
 
 export default axiosInstance;
